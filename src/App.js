936fb86e3a79beae1120c634796d1296e9545bb7
zhongds <1326581318@qq.com>
2017-03-08 22:03:12 +0800

    test demo

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { Form, Select, Input, Button } from 'antd'
+const FormItem = Form.Item
+const Option = Select.Option
+
+class App extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values)
+      }
+    })
+  }
+  handleSelectChange = (value) => {
+    console.log(value)
+    this.props.form.setFieldsValue({
+      note: `Hi, ${value === 'male' ? 'man' : 'lady'}!`,
+    })
+  }
+  render() {
+    const { getFieldDecorator } = this.props.form
+    return (
+      <Form onSubmit={this.handleSubmit}>
+        <FormItem
+          label="Note"
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 8 }}
+        >
+          {getFieldDecorator('note', {
+            rules: [{ required: true, message: 'Please input your note!' }],
+          })(
+            <Input />
+          )}
+        </FormItem>
+        <FormItem
+          label="Gender"
+          labelCol={{ span: 4 }}
+          wrapperCol={{ span: 8 }}
+        >
+          {getFieldDecorator('gender', {
+            rules: [{ required: true, message: 'Please select your gender!' }],
+            onChange: this.handleSelectChange,
+          })(
+            <Select placeholder="Select a option and change input text above">
+              <Option value="male">male</Option>
+              <Option value="female">female</Option>
+            </Select>
+          )}
+        </FormItem>
+        <FormItem
+          wrapperCol={{
+            xs: { span: 8, offset: 0 },
+            sm: { span: 8, offset: 4 },
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </FormItem>
+      </Form>
+    )
+  }
+}
+
+const WrappedApp = Form.create()(App)
+
+export default WrappedApp
